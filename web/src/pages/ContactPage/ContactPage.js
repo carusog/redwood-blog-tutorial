d1af5776956e994216f87425525371b782a914d9
Giuseppe Caruso <giuseppe.caruso@trivago.com>
2021-05-04 18:14:59 +0200

    create Contacts service and implement form

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,17 +1,71 @@
-import { Link, routes } from '@redwoodjs/router'
+import {
+  Form,
+  TextField,
+  Submit,
+  TextAreaField,
+  FieldError,
+  Label,
+} from '@redwoodjs/forms'
+import { useMutation } from '@redwoodjs/web'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+  const [create] = useMutation(CREATE_CONTACT)
+
+  const onSubmit = (data) => {
+    console.log(data)
+    create({ variables: { input: data } })
+  }
+  console.log('render!')
+
   return (
-    <>
-      <h1>ContactPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ContactPage/ContactPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>contact</code>, link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
-    </>
+    <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+      <Label name="name" errorClassName="error">
+        Name
+      </Label>
+      <TextField
+        name="name"
+        validation={{ required: true }}
+        errorClassName="error"
+      />
+      <FieldError className="error" name="name" />
+
+      <Label name="email" errorClassName="error">
+        E-mail
+      </Label>
+      <TextField
+        name="email"
+        validation={{
+          required: true,
+          pattern: {
+            value: /[^@]+@[^.]+\..+/,
+            message: '📧 Please, enter a valid e-mail address',
+          },
+        }}
+        errorClassName="error"
+      />
+      <FieldError className="error" name="email" />
+
+      <Label name="message" errorClassName="error">
+        Message
+      </Label>
+      <TextAreaField
+        name="message"
+        validation={{ required: true }}
+        errorClassName="error"
+      />
+      <FieldError className="error" name="message" />
+
+      <br />
+      <Submit>Save</Submit>
+    </Form>
   )
 }
 
