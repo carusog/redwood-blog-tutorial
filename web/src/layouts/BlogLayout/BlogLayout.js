f20ed79b3e81cdbfe0074c52de7a90a19b383cc5
Giuseppe Caruso <giuseppe.caruso@trivago.com>
2021-05-08 21:44:29 +0200

    add logout button and user email when logged in

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -2,7 +2,7 @@ import { Link, routes } from '@redwoodjs/router'
 import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
-  const { logIn } = useAuth()
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
 
   return (
     <>
@@ -19,12 +19,26 @@ const BlogLayout = ({ children }) => {
               <Link to={routes.contact()}>Contact</Link>
             </li>
             <li>
-              <button onClick={logIn}>Log In</button>
+              <button type="button" onClick={isAuthenticated ? logOut : logIn}>
+                {isAuthenticated ? 'Log Out' : 'Log In'}
+              </button>
             </li>
+            {isAuthenticated && <li>{currentUser.email}</li>}
           </ul>
         </nav>
       </header>
       <main>{children}</main>
+      <pre
+        style={{
+          backgroundColor: 'lightgray',
+          padding: '1.5rem',
+          borderRadius: '1rem',
+          fontSize: '1rem',
+        }}
+      >
+        <h3 style={{ marginTop: '0' }}>currentUser object:</h3>
+        <code>{JSON.stringify(currentUser, null, 4)}</code>
+      </pre>
     </>
   )
 }
